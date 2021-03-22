db0d236f5e7080f63102b4e940664be98b34cf48
jarb29 <jarb29@gmail.com>
2021-03-21 21:42:54 -0300

    type: removing  Auth0 Body: Updating

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 
 
